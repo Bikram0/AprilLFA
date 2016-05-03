4b22fb05b1ada8f0827419631770ffb6ca90300f
Bikram Guvaju <bguvaju@gmail.com>
2016-05-03 08:41:16 +0545

    first commit

---
+++ LFAConsoleApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LFAConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine("Welcome to the Leapfrog Console Appication");
+                Console.ReadLine();
+                Console.WriteLine("Enter the UserName");
+                string strUserName = Console.ReadLine();
+                Console.WriteLine("Enter the Password");
+                string strPassword = Console.ReadLine();
+                if ((strUserName == "admin") && (strPassword == "admin"))
+                {
+                    Console.WriteLine("You are authenticated to enter to the Leap Frog User Management");
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Please Enter Valid Username and Password");
+                }
+            }
+            while (true);
+    
+        }
+    }
+}
